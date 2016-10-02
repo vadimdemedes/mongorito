155541a0a8c61a06592698d6c441781065af723b
vdemedes <vdemedes@gmail.com>
2016-10-02 17:51:55 +0300

    remove babel

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 'use strict';
 
-const {
-	Timestamp,
-	ObjectId,
-	MinKey,
-	MaxKey,
-	DBRef,
-	Long
-} = require('mongodb');
+const mongodb = require('mongodb');
 
 module.exports = require('./lib/database');
 module.exports.Model = require('./lib/model');
 
-module.exports.Timestamp = Timestamp;
-module.exports.ObjectId = ObjectId;
-module.exports.MinKey = MinKey;
-module.exports.MaxKey = MaxKey;
-module.exports.DBRef = DBRef;
-module.exports.Long = Long;
+module.exports.Timestamp = mongodb.Timestamp;
+module.exports.ObjectId = mongodb.ObjectId;
+module.exports.MinKey = mongodb.MinKey;
+module.exports.MaxKey = mongodb.MaxKey;
+module.exports.DBRef = mongodb.DBRef;
+module.exports.Long = mongodb.Long;
